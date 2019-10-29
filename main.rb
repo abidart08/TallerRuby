ae3e767ec55eb86e6212cdec62594d4429378931
Agustin <agustinbidart_97@hotmail.com>
2019-10-29 13:20:58 -0300

    Agrego la clase Value con sus getters y setters correspondientes

--- main.rb
+++ main.rb
@@ -7,3 +7,39 @@ while session = server.accept
   session.print "Hello world! The time is #{Time.now}" 
   session.close
 end
+
+class Memcached
+  @values
+  def Memcached()
+    values = Array.new(100)
+  end
+
+
+
+class Value
+  @flag
+  @time
+  @size
+  def Value(flag, time, size)
+    setFlag(flag)
+    setTime(time)
+    setSize(size)
+  end
+  def setFlag(unFlag)
+    flag = unFlag
+  end
+  def setTime(unTime)
+    time = unTime
+  end
+  def setSize(unSize)
+    size = unSize
+  end
+  def getFlag()
+    return flag
+  end
+  def getTime()
+    return time
+  def getSize()
+    return size
+  end
+  
